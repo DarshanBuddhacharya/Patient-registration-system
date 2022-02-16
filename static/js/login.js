6c0a4792faac20d1b82513666ace80c621062d87
Darshan <np03a190140@heraldcollege.edu.np>
2022-02-16 16:33:12 +0545

    added account button on navbar

--- static/js/login.js
+++ static/js/login.js
@@ -18,3 +18,26 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+$(".profile .icon_wrap").click(function(){
+  $(this).parent().toggleClass("active");
+  $(".notifications").removeClass("active");
+  if (($(e.target).closest('.profile').length == 0) && ($(e.target).closest('.profile').length == 0)){
+	  $(".profile").removeClass("active");
+  }
+});
+
+$(".notifications .icon_wrap").click(function(){
+  $(this).parent().toggleClass("active");
+   $(".profile").removeClass("active");
+});
+
+
+//  $(document).on('click', function (e) {
+//     if (($(e.target).closest('#menu').length == 0) && ($(e.target).closest('#open_menu').length == 0)) {
+//       if (isMenuOpen == true) {
+//         isMenuOpen = false;
+//         $("#menu").hide();
+//       }
+//     }
+//   });
